aebe961e560361f6e6b85177ad870cffa3f12852
Roshan Sharma <imrosun@gmail.com>
2022-06-13 16:17:19 +0530

    Speed

---
+++ src/Uploaded/Speed.java
@@ -0,0 +1,39 @@
+// To find out how much time will it take if you increase the speed of a video.
+package Uploaded;
+import java.util.Scanner;
+
+public class Speed {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hour = sc.nextInt();
+        int min = sc.nextInt();
+        float sp = sc.nextFloat();
+        System.out.println(speed(hour,min,sp));
+    }
+    public static float speed(int h, int m, float s){
+        while(h<12) {
+            h = h * 60;
+            h = h+ m;
+            if(s<=2.0f){
+                s = h * s;
+                s = s - h;
+                return s;
+            }
+        }
+        if(s<=2.0f){
+            s = h * s;
+            s = s - h;
+            return s;
+        }
+        h = h + m;
+        return s;
+    }
+//    if(h<12) {
+//        h = h * 60;
+//        h = h + m;
+//    } else {
+//        h = h + m;
+//    }
+//        return h;
+//}
+}
