f8508d14e5ea80554b980ac1cc2f0448125d264d
Roshan Sharma <imrosun@gmail.com>
2022-07-19 16:51:14 +0530

    Added Fibo

---
+++ src/Uploaded/Add.java
@@ -0,0 +1,18 @@
+package Uploaded;
+
+public class Add {
+    public static void main(String[] args) {
+//        int x=4;
+//        int y=3;
+//        System.out.println(++x * y--);
+//        boolean yes = true;
+//        boolean no = false;
+//        System.out.println(yes || no);
+
+        String a = "Roshan";
+        String b = "Roshan";
+        String c = new String ("Roshan");
+        System.out.println(String.valueOf(a==c));
+        System.out.println(String.valueOf(a.equals(c)));
+    }
+}
